c2c1dec2a5d89b605328c31647b484da7fedc899
Renato Sousa <renato3.al@gmail.com>
2023-08-02 08:42:42 -0300

    Utilizando Helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get "welcome/index"
+  # get '/inicio', to: 'welcome#index'
+
   resources :coins
+  # get "coins", to: "coins#index"
+
+  root to: "welcome#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
